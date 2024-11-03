2f9ca2bade19a4710b42ec8e8a856232c34fa7cc
Hope <boukhatemabdelilah420@gmail.com>
2024-11-03 10:40:29 +0100

    first commit 03/11/2024

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Diagram from "@/Components/diagram";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen">
+      <Diagram />
+    </div>
+  );
+}
